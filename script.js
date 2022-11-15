16ea6b0f12923fa2bbbda53ad6fb3f75e8cd210d
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-15 14:58:39 +0100

    Aggiunto ciclo FOR

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let arrayListaSpesa = ["Pane", "Pasta", "Latte", "Uova"];
 
+for (let i = 0; i < arrayListaSpesa.length; i++){
+    console.log(
+        `
+        ---LISTA SPESA CICLO FOR:---
+        ${arrayListaSpesa};
+        `
+    );
+}
+
+
+
